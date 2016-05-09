482c239423a454f89162885f813fa6ddbf28c4f5
Erwin <ejhessing@gmail.com>
2016-05-10 00:03:33 +1200

    Sudo code for jQuery

---
+++ app.js
@@ -0,0 +1,11 @@
+// Hide hints 
+
+//when event happens on password input
+	// find out if password is valid
+		//hide hint if valid
+		//else show hint
+
+// when event happens on confirmation
+	//find out if password and confirmation match
+		//hide hint if match
+		//else show hint 
