d5c55c848348176cf7d423a536c48b1a18d0759e
Erwin <ejhessing@gmail.com>
2016-05-10 12:11:03 +1200

    Done jQuery

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 // Hide hints 
+$("form span").hide();
+
+function passwordEvent () {
+	if($(this).val().length > 8){
+		//hide hint if valid
+		$(this).next().hide();
+	} else {
+		//else show hint
+	$(this).next().show();
+
+	}
+}
+
+function confirmPasswordEvent(){
+	//find out if password and confirmation match
+	if($("#password").val() === $("#confirm_password").val()){
+		//hide hint if match
+		$("#confirm_password").next().hide();
+
+	} else {
+		//else show hint 
+		$("#confirm_password").next().show();
+	}	
+}
+
+
 
 //when event happens on password input
-	// find out if password is valid
-		//hide hint if valid
-		//else show hint
+// find out if password is valid
+$("#password").focus(passwordEvent).keyup(passwordEvent);
+	
+	
+		
 
 // when event happens on confirmation
-	//find out if password and confirmation match
-		//hide hint if match
-		//else show hint 
+$("#confirm_password").focus(confirmPasswordEvent).keyup(confirmPasswordEvent);
